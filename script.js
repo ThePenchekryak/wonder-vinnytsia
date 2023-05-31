30c68c764de7f862b0c2ab59349f02d44149ea58
Mykola Chernysh <sfairat.rahl@gmail.com>
2023-06-01 00:59:09 +0300

    Initialization

---
+++ script.js
@@ -0,0 +1,12 @@
+function showTextBusha() {
+  let text = document.querySelectorAll(".info")[0]
+  text.classList.toggle("hidden")
+}
+function showTextQuarry() {
+  let text = document.querySelectorAll(".info")[1]
+  text.classList.toggle("hidden")
+}
+function showTextRiver() {
+  let text = document.querySelectorAll(".info")[2]
+  text.classList.toggle("hidden")
+}
